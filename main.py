c2bd5ba38378a0c01f27f90603d7e8efc7e2a5a6
Robert Duckworth <robduckyworth@gmail.com>
2020-01-14 19:58:38 +0000

    added frame functions for finding sources

--- main.py
+++ main.py
@@ -52,18 +52,43 @@ def create_dirs(args):
         open(args.directory + "/CMakeLists.txt", "w").close()
     else:
         print("CMakeLists.txt found at root, not creating a new one.")
-
-    # touch cml
     for dir in top_level:
-        for pattern in ["src", "source", "lib"]:
-            if pattern == dir:
-                print(dir)
-                print("potential source dir found")
+        check_src(dir)
     print(top_level)
 
+def check_src(dirname):
+    for pattern in ["src", "source", "lib"]:
+        if pattern == dirname:
+            print(dir)
+            print("potential source dir found")
 
-def add_targets(name, type):
-    pass
+def find_header_files(dir):
+    head_ext = [".h", ".hpp", ".mod"]
+    found_headers = []
+    dir_list = os.listdir(dir)
+    for file in dir_list:
+        for ext in head_ext:
+            if ext in file:
+                found_sources.append(file)
+    return found_headers
+
+def find_sources(dir):
+    extensions = [".cpp", ".f", ".f90", ".F90", ".c", ".py",]
+    found_sources = []
+    dir_list = os.listdir(dir)
+    for file in dir_list:
+        for ext in extensions:
+            if ext in file:
+                found_sources.append(file)
+    return found_sources
+
+def add_targets(name, type, source_list):
+        if type == "lib":
+            target_str = str("add_library(%s %s %s)" % (name, type, source_list))
+        else:
+            target_str = str("add_executable(%s %s %s)" % (name, type, source_list))
+
+        print(project_str)
 
 def add_subdirs():
     pass
