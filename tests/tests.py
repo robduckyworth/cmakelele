79cdef5ba4de811fedf5c0e4e75cac0356db2500
Robert Duckworth <robduckyworth@gmail.com>
2020-01-28 19:14:38 +0000

    testing outline

---
+++ tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+    def test_enable_lang(self):
+        pass
+    def test_gen_version(self):
+        pass
+    def test_gen_project(self):
+        pass
+    def test_create_dirs(self):
+        pass
+    def test_find_headers(self):
+        pass
+    def test_find_sources(self):
+        pass
+    def test_add_targets(self):
+        pass
+    def test_add_subdirs(self):
+        pass
